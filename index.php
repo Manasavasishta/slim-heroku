cd24643641df0ec88511c4c60d7230798f9e4975
Manasa <Manasavasishta@gmail.com>
2015-05-01 19:59:16 -0400

    files

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 
 $app = new \Slim\Slim();
 
-$app->get('/hello/:name',function($name){
-           echo "Hello,$name";
+$app->get('/',function($name){
+           echo "Foo!";
 });
 $app->run();
 ?>
